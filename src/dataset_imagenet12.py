994a28b7045e3d0c0d5ac5a87175a5e2bcf86852
sanyald <deepayan.sanyal@vanderbilt.edu>
2022-07-13 13:31:09 -0500

    Load in-12 train fractions

--- src/dataset_imagenet12.py
+++ src/dataset_imagenet12.py
@@ -9,10 +9,11 @@ class DataLoaderGeneric(torch.utils.data.Dataset):
 	"""
 	This class
 	"""
-	def __init__(self, root, train=True, transform=None):
+	def __init__(self, root, train=True, transform=None, fraction=1.0):
 		self.train = train
 		self.transform = transform
 		self.root = root
+		self.fraction = fraction
 		
 		if self.train:
 			self.images_file = self.root + "train.pickle"
@@ -22,13 +23,28 @@ class DataLoaderGeneric(torch.utils.data.Dataset):
 			self.labels_file = self.root + "test.csv"
 		self.images = pickle.load(open(self.images_file, "rb"))
 		self.labels = list(csv.DictReader(open(self.labels_file, "r")))
-		
+		if self.train:
+			if self.fraction < 1.0:
+				len_all_images = len(self.images)
+				len_train_images = int(len_all_images * self.fraction)
+				rng = np.random.default_rng(0)
+				self.selected_indices = rng.choice(len_all_images, len_train_images, replace=False)
+			else:
+				self.selected_indices = np.arange(len(self.images))
+				
 	def __len__(self):
-		return len(self.images)
+		if self.train:
+			return len(self.selected_indices)
+		else:
+			return len(self.images)
 
-	def __getitem__(self, item):
+	def __getitem__(self, index):
+		if self.train:
+			item = self.selected_indices[index]
+		else:
+			item = index
 		im = np.array(cv2.imdecode(self.images[item], 3))
 		label = int(self.labels[item]["Class ID"])
 		if self.transform is not None:
 			im = self.transform(im)
-		return item, im, label
+		return index, im, label
