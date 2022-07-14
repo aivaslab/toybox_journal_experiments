c39478d049ec13f21c5be82a22ae28269c4c3525
sanyald <deepayan.sanyal@vanderbilt.edu>
2022-07-14 18:55:26 -0500

    Added colorbar to umap and OOP'd code

--- src/umap_util.py
+++ src/umap_util.py
@@ -9,6 +9,9 @@ import torchvision.transforms as transforms
 import umap.umap_ as umap
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
+import matplotlib.colors as colors
+import numpy as np
+import torch.utils.data as torchdata
 
 import dataset_imagenet12
 
@@ -16,14 +19,26 @@ IN12_DATA_PATH = "../data_12/IN-12/"
 IN12_MEAN = (0.485, 0.456, 0.406)
 IN12_STD = (0.229, 0.224, 0.225)
 
-toybox_classes = ['airplane', 'ball', 'car', 'cat', 'cup', 'duck', 'giraffe', 'helicopter', 'horse', 'mug', 'spoon',
-                  'truck']
+color_maps = cm.get_cmap('tab20b')
+COLORS = {
+     0: color_maps(0),  # airplane
+     1: color_maps(4),  # ball
+     2: color_maps(1),  # car
+     3: color_maps(8),  # cat
+     4: color_maps(5),  # cup
+     5: color_maps(9),  # duck
+     6: color_maps(10),  # giraffe
+     7: color_maps(2),  # helicopter
+     8: color_maps(11),  # horse
+     9: color_maps(6),  # mug
+     10: color_maps(7),  # spoon
+     11: color_maps(3),  # truck
+     }
 
 
-class Model:
+class UMapFromModel:
     """
-    This class implements KNN classifier for given training data and test data.
-    Can be subclassed for ZSL and FSL
+    This class implements basic backbone of a model and generates activations for specified datasets
     """
     
     def __init__(self, model_path):
@@ -35,13 +50,19 @@ class Model:
         self.backbone.load_state_dict(torch.load(self.model_path))
         self.backbone.cuda()
         self.backbone.eval()
+        self.reducer = umap.UMAP(
+            n_neighbors=15,
+            min_dist=0.15,
+            n_components=2,
+            metric='cosine'
+        )
         
     def get_activations(self, data):
         """
         This method gets the activations for the training data.
         """
         len_train_data = len(data)
-        data_loader = torch.utils.data.DataLoader(data, batch_size=64, shuffle=False, num_workers=4)
+        data_loader = torchdata.DataLoader(data, batch_size=64, shuffle=False, num_workers=4)
         
         activations = torch.zeros(len_train_data, self.fc_size)
         labels = torch.zeros(len_train_data, dtype=torch.float32)
@@ -60,40 +81,59 @@ class Model:
             mean_act = torch.mean(activations_cl, dim=0, keepdim=False)
             mean_activations[cl] = mean_act
         all_activations = torch.cat([activations, mean_activations], dim=0)
-        print(activations.shape, mean_activations.shape, all_activations.shape)
         return activations.numpy(), labels.numpy()
+    
+    def fit_data(self, activations):
+        """
+        Fit the UMAP reducer to the activation and return embedding
+        """
+        self.reducer.fit(activations)
+        embedding = self.reducer.transform(activations)
+        return embedding
+    
+    def transform_data(self, activations):
+        """
+        Get embeddings for activations using reducer fit previously
+        """
+        embedding = self.reducer.transform(activations)
+        return embedding
+    
+    @staticmethod
+    def plot(embeddings, labels, markers):
+        """
+        Draw the scatter plot for the embeddings
+        """
+        classes_ordered_by_color = ['airplane', 'car', 'helicopter', 'truck', 'ball', 'cup', 'mug', 'spoon', 'cat',
+                                    'duck', 'giraffe', 'horse']
+        cmap = colors.ListedColormap([color_maps(i) for i in range(12)])
+        bounds = list(COLORS.keys()) + [12]
+        norm = colors.BoundaryNorm(bounds, 12)
+
+        fig, ax = plt.subplots()
+        for i in range(len(embeddings)):
+            ax.scatter(embeddings[i][:, 0], embeddings[i][:, 1], marker=markers[i], s=15,
+                       c=[COLORS[int(labels[i][idx])] for idx in range(len(labels[i]))])
+
+        cb = fig.colorbar(cm.ScalarMappable(norm=norm, cmap=cmap), orientation='vertical')
+        cb.set_ticks(ticks=np.arange(12) + 0.5, labels=classes_ordered_by_color)
+        fig.set_tight_layout(True)
+        plt.show()
 
 
 if __name__ == "__main__":
     transform = transforms.Compose([transforms.ToPILImage(), transforms.ToTensor(),
                                     transforms.Normalize(mean=IN12_MEAN, std=IN12_STD)])
     in12_train_data = dataset_imagenet12.DataLoaderGeneric(root=IN12_DATA_PATH, train=True, transform=transform,
-                                                           fraction=0.05)
-    color_maps = cm.get_cmap('tab20b')
-    COLORS = {
-        0: color_maps(0),   # airplane
-        1: color_maps(4),   # ball
-        2: color_maps(1),   # car
-        3: color_maps(8),   # cat
-        4: color_maps(5),   # cup
-        5: color_maps(9),   # duck
-        6: color_maps(10),  # giraffe
-        7: color_maps(2),    # helicopter
-        8: color_maps(11),    # horse
-        9: color_maps(6),    # mug
-        10: color_maps(7),   # spoon
-        11: color_maps(3),   # truck
-    }
-    
-    model = Model(model_path="../out/in12_baseline/backbone_trainer_resnet18_backbone.pt")
+                                                           fraction=1.0)
+
+    in12_test_data = dataset_imagenet12.DataLoaderGeneric(root=IN12_DATA_PATH, train=False, transform=transform,
+                                                          fraction=0.05)
+
+    model = UMapFromModel(model_path="../out/in12_baseline/backbone_trainer_resnet18_backbone.pt")
     in12_train_activations, in12_train_labels = model.get_activations(data=in12_train_data)
-    reducer = umap.UMAP()
-    reducer.fit(in12_train_activations)
-    embedding = reducer.transform(in12_train_activations)
-    plt.scatter(embedding[:, 0], embedding[:, 1], marker='.',
-                c=[COLORS[int(in12_train_labels[i])] for i in range(len(in12_train_labels))])
-    import numpy as np
-    # plt.colorbar(boundaries=np.arange(13)-0.5)
-    plt.show()
-            
-    
+    in12_test_activations, in12_test_labels = model.get_activations(data=in12_test_data)
+
+    train_embeddings = model.fit_data(in12_train_activations)
+    test_embeddings = model.transform_data(in12_test_activations)
+    model.plot(embeddings=[train_embeddings, test_embeddings], labels=[in12_train_labels, in12_test_labels],
+               markers=['.', 'x'])
