bd7ce753fff33efc5a717225a0245df18edc446b
sanyald <deepayan.sanyal@vanderbilt.edu>
2022-07-15 17:43:43 -0500

    train entropy model

--- src/entropy.py
+++ src/entropy.py
@@ -9,9 +9,11 @@ import torch.utils.data as torchdata
 import numpy as np
 import tqdm
 import torch.utils.tensorboard as tb
+import argparse
 
 import dataset_imagenet12
 import dataset_toybox
+import utils
 
 TOYBOX_DATA_PATH = "../data_12/Toybox/"
 IN12_DATA_PATH = "../data_12/IN-12/"
@@ -26,7 +28,8 @@ class EntModel:
     """
         Model that learns from entropy on target distribution
         """
-    def __init__(self, model_path):
+    def __init__(self, model_path, exp_args):
+        self.args = exp_args
         self.model_path = model_path
         self.backbone = models.resnet18(pretrained=False)
         self.fc_size = self.backbone.fc.in_features
@@ -50,16 +53,24 @@ class EntModel:
         Train loaded model with data provided
         """
         tb_writer = tb.SummaryWriter(log_dir="../runs/entmod/")
-        data_loader = torchdata.DataLoader(data, batch_size=64, num_workers=4, shuffle=True, pin_memory=True,
-                                           persistent_workers=True)
-        ent_data_loader = torchdata.DataLoader(ent_train_data, batch_size=64, num_workers=4, shuffle=True)
+        data_loader = torchdata.DataLoader(data, batch_size=self.args['batch_size'], num_workers=4, shuffle=True,
+                                           pin_memory=True, persistent_workers=True)
+        ent_data_loader = torchdata.DataLoader(ent_train_data, batch_size=self.args['batch_size'], num_workers=4,
+                                               shuffle=True)
         ent_loader_iter = iter(ent_data_loader)
-        optimizer = torch.optim.SGD(self.fc.parameters(), lr=1e-3, weight_decay=1e-6)
+        optimizer = torch.optim.SGD(self.fc.parameters(), lr=self.args['lr'], weight_decay=1e-6)
         for params in self.fc.parameters():
             params.requires_grad = True
-        num_epochs = 10
+        if self.args['backbone']:
+            for params in self.backbone.parameters():
+                params.requires_grad = True
+            optimizer.add_param_group({'params': self.backbone.parameters()})
+            
+        num_epochs = self.args['epochs']
         entropy = self.run_dataset(eval_data)
         self.fc.train()
+        if self.args['backbone']:
+            self.backbone.train()
         total_batches = 0
         # tb_writer.add_scalar(tag='Entropy', scalar_value=entropy, global_step=total_batches)
         for ep in range(num_epochs):
@@ -72,41 +83,50 @@ class EntModel:
                 feats = self.backbone.forward(images)
                 logits = self.fc.forward(feats)
                 loss = nn.CrossEntropyLoss()(logits, labels)
+            
                 try:
                     _, images_ent, _ = next(ent_loader_iter)
                 except StopIteration:
                     ent_loader_iter = iter(ent_data_loader)
                     _, images_ent, _ = next(ent_loader_iter)
-                
+            
                 images_ent = images_ent.cuda()
-                # feats_ent = self.backbone.forward(images_ent)
-                # activations_ent = self.fc.forward(feats_ent)
-                
+                feats_ent = self.backbone.forward(images_ent)
+                activations_ent = self.fc.forward(feats_ent)
+                train_entropy = torch.mean(self.calc_entropy(activations_ent))
+                combined_loss = loss + self.args['lambda'] * train_entropy
                 num_batches += 1
                 total_loss += loss.item()
                 total_batches += 1
-                tqdm_bar.set_description("Epoch: {}/{}  LR: {:.4f}  CE Loss: {:.4f} Train Entropy: Test Entropy: {:.6f}".
+                tqdm_bar.set_description("Epoch: {}/{}  LR: {:.4f}  CE Loss: {:.4f} Train Entropy: {:.4f},  "
+                                         "Total Loss: {:.4f}  Test Entropy: {:.6f}".
                                          format(ep+1, num_epochs, optimizer.param_groups[0]['lr'],
-                                                total_loss/num_batches, entropy))
-        
-                loss.backward()
+                                                total_loss/num_batches, train_entropy.item(), combined_loss.item(),
+                                                entropy))
+            
+                combined_loss.backward()
                 optimizer.step()
-                if (num_batches+1) % 20 == 0:
-                    entropy = self.run_dataset(eval_data)
-                    self.fc.train()
+                # if (num_batches+1) % 20 == 0:
+                #     entropy = self.run_dataset(eval_data)
+                #     self.fc.train()
                 tb_writer.add_scalar(tag='Entropy', scalar_value=entropy, global_step=total_batches)
-                tb_writer.add_scalar(tag='Loss/Toybox_Train', scalar_value=total_loss/num_batches, global_step=total_batches)
-    
+                tb_writer.add_scalar(tag='Loss/Toybox_Train', scalar_value=total_loss/num_batches,
+                                     global_step=total_batches)
+        
             entropy = self.run_dataset(eval_data)
             tb_writer.add_scalar(tag='Entropy', scalar_value=entropy, global_step=total_batches)
             print("Average Entropy: {}".format(entropy))
-
+        
             test_loss = self.get_loss(eval_data)
             tb_writer.add_scalar(tag='Loss/IN12_Test', scalar_value=test_loss, global_step=total_batches)
             print("Test Loss: {}".format(test_loss))
-    
+        
+            self.get_acc(eval_data)
+        
             self.fc.train()
-            tb_writer.close()
+            if self.args['backbone']:
+                self.backbone.train()
+        tb_writer.close()
             
     def calc_entropy(self, activations):
         """
@@ -159,10 +179,49 @@ class EntModel:
                 batches += 1
     
         return total_loss / batches
+
+    def get_acc(self, data):
+        """
+        Pass dataset through network and calculate accuracy
+        """
+        data_loader = torchdata.DataLoader(data, batch_size=64, num_workers=4, pin_memory=False,
+                                           persistent_workers=True)
+    
+        self.backbone.eval()
+        self.fc.eval()
+        top1acc = 0.0
+        tot_train_points = 0
+        for _, images, labels in data_loader:
+            images, labels = images.cuda(), labels.cuda()
+            with torch.no_grad():
+                feats = self.backbone(images)
+                activations = self.fc(feats)
+                top, pred = utils.calc_accuracy(activations, labels, topk=(1, ))
+                top1acc += top[0].item() * pred.shape[0]
+                tot_train_points += pred.shape[0]
+        top1acc /= tot_train_points
+        print("Accuracy on test set: {}".format(top1acc))
+    
+        return top1acc
                 
         
+def get_parser():
+    """
+    Create parser and return arguments for experiment
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--lambda", "-l", default=1, type=int)
+    parser.add_argument("--epochs", "-e", default=10, type=int)
+    parser.add_argument("--backbone", "-bb", default=False, action='store_true')
+    parser.add_argument("--lr", "-lr", default=0.1, type=float)
+    parser.add_argument("--batch-size", "-b", default=64, type=int)
+    
+    return parser.parse_args()
+    
+    
 if __name__ == "__main__":
-    model = EntModel(model_path="../out/toybox_baseline/")
+    args = vars(get_parser())
+    model = EntModel(model_path="../out/toybox_baseline/", exp_args=args)
     transform_in12 = transforms.Compose([transforms.ToPILImage(), transforms.ToTensor(),
                                          transforms.Normalize(mean=IN12_MEAN, std=IN12_STD)])
     in12_test_data = dataset_imagenet12.DataLoaderGeneric(root=IN12_DATA_PATH, train=False, transform=transform_in12)
