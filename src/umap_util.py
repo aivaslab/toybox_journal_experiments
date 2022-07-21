63a2d954db0c36c14c4daeef2f8da09fbba08c6d
sanyald <deepayan.sanyal@vanderbilt.edu>
2022-07-21 15:02:35 -0500

    code to run umap easily

--- src/umap_util.py
+++ src/umap_util.py
@@ -12,6 +12,7 @@ import matplotlib.cm as cm
 import matplotlib.colors as colors
 import numpy as np
 import torch.utils.data as torchdata
+import os
 
 import dataset_imagenet12
 import dataset_toybox
@@ -62,12 +63,18 @@ class UMapFromModel:
             metric='cosine'
         )
         
+    def reset_reducer(self, n_nbrs, min_d, metric):
+        """
+        This method resets the reducer based on the provided arguments
+        """
+        self.reducer = umap.UMAP(n_neighbors=n_nbrs, min_dist=min_d, metric=metric, n_components=2)
+        
     def get_activations(self, data):
         """
         This method gets the activations for the training data.
         """
         len_train_data = len(data)
-        data_loader = torchdata.DataLoader(data, batch_size=64, shuffle=False, num_workers=4)
+        data_loader = torchdata.DataLoader(data, batch_size=256, shuffle=False, num_workers=4)
         
         activations = torch.zeros(len_train_data, self.fc_size)
         labels = torch.zeros(len_train_data, dtype=torch.float32)
@@ -85,14 +92,14 @@ class UMapFromModel:
             activations_cl = torch.squeeze(activations[indices_cl])
             mean_act = torch.mean(activations_cl, dim=0, keepdim=False)
             mean_activations[cl] = mean_act
-        all_activations = torch.cat([activations, mean_activations], dim=0)
+        # all_activations = torch.cat([activations, mean_activations], dim=0)
         return activations.numpy(), labels.numpy()
     
-    def fit_data(self, activations):
+    def fit_data(self, activations, labels=None):
         """
         Fit the UMAP reducer to the activation and return embedding
         """
-        self.reducer.fit(activations)
+        self.reducer.fit(activations, labels)
         embedding = self.reducer.transform(activations)
         return embedding
     
@@ -104,7 +111,7 @@ class UMapFromModel:
         return embedding
     
     @staticmethod
-    def plot(embeddings, labels, markers):
+    def plot(embeddings, labels, markers, out_path):
         """
         Draw the scatter plot for the embeddings
         """
@@ -114,23 +121,41 @@ class UMapFromModel:
         bounds = list(COLORS.keys()) + [12]
         norm = colors.BoundaryNorm(bounds, 12)
 
-        fig, ax = plt.subplots()
+        fig, ax = plt.subplots(dpi=600)
         for i in range(len(embeddings)):
-            ax.scatter(embeddings[i][:, 0], embeddings[i][:, 1], marker=markers[i], s=15,
+            ax.scatter(embeddings[i][:, 0], embeddings[i][:, 1], marker=markers[i], s=2,
                        c=[COLORS[int(labels[i][idx])] for idx in range(len(labels[i]))])
 
         cb = fig.colorbar(cm.ScalarMappable(norm=norm, cmap=cmap), orientation='vertical')
         cb.set_ticks(ticks=np.arange(12) + 0.5, labels=classes_ordered_by_color)
         fig.set_tight_layout(True)
-        plt.show()
+        fig.savefig(out_path, dpi=600, bbox_inches='tight')
+        plt.close()
 
 
-def get_umap_two_datasets(model_path, data_1, data_2):
+def get_umap_two_datasets(model_path, out_dir_path, data_1, data_2):
     """
     This method can be used to get the UMAPS for two datasets.
     The UMAP transform will be learned based on data_1 and data_2 will be treated
     as test data.
     """
+    nbrs = [5, 10, 20, 50, 75, 100, 200]
+    min_ds = [0.02, 0.05, 0.1, 0.2, 0.5]
+    os.makedirs(out_dir_path, exist_ok=True)
+    
+    mapper = UMapFromModel(model_path=model_path)
+    data_1_activations, data_1_labels = mapper.get_activations(data=data_1)
+    data_2_activations, data_2_labels = mapper.get_activations(data=data_2)
+    for nbr in nbrs:
+        for d in min_ds:
+            mapper.reset_reducer(n_nbrs=nbr, min_d=d, metric='cosine')
+            data_1_embeddings = mapper.fit_data(activations=data_1_activations, labels=data_1_labels)
+            data_2_embeddings = mapper.transform_data(activations=data_2_activations)
+            plot_path = out_dir_path + "_".join(["umap", str(nbr), str(d), "cosine"]) + ".png"
+            mapper.plot(embeddings=[data_1_embeddings, data_2_embeddings], labels=[data_1_labels, data_2_labels],
+                        markers=['.', '+'], out_path=plot_path)
+            print("Saved plot into {}".format(plot_path))
+    
 
 if __name__ == "__main__":
     transform_in12 = transforms.Compose([transforms.ToPILImage(), transforms.ToTensor(),
@@ -147,7 +172,10 @@ if __name__ == "__main__":
                                                      transform=transform_toybox)
     toybox_test_data = dataset_toybox.ToyboxDataset(root=TOYBOX_DATA_PATH, rng=np.random.default_rng(0), train=False,
                                                     transform=transform_toybox)
-
+    get_umap_two_datasets(model_path="../out/in12_baseline/backbone_trainer_resnet18_backbone.pt",
+                          out_dir_path="../out/in12_baseline/umap/",
+                          data_1=in12_train_data, data_2=in12_test_data)
+    '''
     model = UMapFromModel(model_path="../out/simclr_trial_2/ssl_resnet18_backbone.pt")
     in12_train_activations, in12_train_labels = model.get_activations(data=in12_train_data)
     in12_test_activations, in12_test_labels = model.get_activations(data=in12_test_data)
@@ -160,3 +188,4 @@ if __name__ == "__main__":
 
     model.plot(embeddings=[train_embeddings, test_embeddings], labels=[in12_train_labels, in12_test_labels],
                markers=['.', 'x'])
+    '''
