b059833e161b3c0c1312c08e3a737193e53eb4f2
sanyald <deepayan.sanyal@vanderbilt.edu>
2022-07-26 17:25:42 -0500

    dann changes

--- src/dann.py
+++ src/dann.py
@@ -11,29 +11,26 @@ import torch.nn as nn
 from torch.autograd import Function
 import tqdm
 import torch.utils.tensorboard as tb
+import math
 
 import utils
 
 
 class GradReverse(Function):
     """
-    Gradient Reversal method
+    Gradient Reversal module
     """
     
     @staticmethod
-    def forward(ctx, i):
+    def forward(ctx, i, alpha):
         """forward method"""
+        ctx.alpha = alpha
         return i.view_as(i)
     
     @staticmethod
     def backward(ctx, grad_output):
         """backward method"""
-        return grad_output * -1
-
-
-def grad_reverse(x):
-    """sdf"""
-    return GradReverse.apply(x)
+        return grad_output.neg() * ctx.alpha, None
 
 
 class Network(nn.Module):
@@ -45,25 +42,32 @@ class Network(nn.Module):
         super().__init__()
         self.backbone = nn.Sequential(nn.Conv2d(in_channels=3, out_channels=64, kernel_size=(5, 5)),
                                       nn.ReLU(),
+                                      nn.Dropout(p=0.1),
                                       nn.BatchNorm2d(num_features=64),
                                       nn.MaxPool2d(kernel_size=(3, 3), stride=(2, 2)),
                                       nn.Conv2d(in_channels=64, out_channels=64, kernel_size=(5, 5)),
                                       nn.ReLU(),
+                                      nn.Dropout(p=0.25),
                                       nn.BatchNorm2d(num_features=64),
                                       nn.MaxPool2d(kernel_size=(3, 3), stride=(2, 2)),
                                       nn.Conv2d(in_channels=64, out_channels=128, kernel_size=(4, 4)),
+                                      nn.Dropout(p=0.25),
                                       nn.BatchNorm2d(num_features=128)
                                       )
         self.classifier = nn.Sequential(nn.Linear(128, 3072), nn.ReLU(), nn.BatchNorm1d(num_features=3072),
+                                        nn.Dropout(),
                                         nn.Linear(3072, 2048), nn.ReLU(), nn.BatchNorm1d(num_features=2048),
+                                        nn.Dropout(),
                                         nn.Linear(2048, 10)
                                         )
         self.domain_classifier = nn.Sequential(nn.Linear(128, 1024), nn.ReLU(), nn.BatchNorm1d(num_features=1024),
+                                               nn.Dropout(),
                                                nn.Linear(1024, 1024), nn.ReLU(), nn.BatchNorm1d(num_features=1024),
+                                               nn.Dropout(),
                                                nn.Linear(1024, 1)
                                                )
     
-    def forward(self, x, source_size):
+    def forward(self, x, source_size, alpha):
         """
         Forward method
         """
@@ -71,7 +75,7 @@ class Network(nn.Module):
         feats = feats.view(feats.size(0), -1)
         feats_source = feats[:source_size]
         logits = self.classifier(feats_source)
-        grad_reversed_out = grad_reverse(feats)
+        grad_reversed_out = GradReverse.apply(feats, alpha)
         dom = self.domain_classifier(grad_reversed_out)
         return feats, logits, dom.squeeze()
 
@@ -81,16 +85,18 @@ class Experiment:
     Class used to run the experiments
     """
     
-    def __init__(self, train_on_svhn):
+    def __init__(self, train_on_svhn, dual_train=True):
         self.train_on_svhn = train_on_svhn
+        self.dual_train = dual_train
         self.net = Network()
         
-        self.optimizer = torch.optim.SGD(self.net.backbone.parameters(), lr=0.01, weight_decay=1e-5)
+        self.optimizer = torch.optim.SGD(self.net.backbone.parameters(), lr=0.01, weight_decay=1e-5, momentum=0.9)
         self.optimizer.add_param_group({'params': self.net.classifier.parameters()})
-        self.optimizer.add_param_group({'params': self.net.domain_classifier.parameters()})
+        if self.dual_train:
+            self.optimizer.add_param_group({'params': self.net.domain_classifier.parameters()})
         mnist_transform = transforms.Compose([transforms.Grayscale(3),
-                                              transforms.ColorJitter(hue=0.2, contrast=0.5, saturation=0.5,
-                                                                     brightness=0.3),
+                                              # transforms.ColorJitter(hue=0.2, contrast=0.5, saturation=0.5,
+                                              # brightness=0.3),
                                               transforms.Resize(32),
                                               transforms.ToTensor(),
                                               transforms.Normalize(mean=(0.1307, 0.1307, 0.1307),
@@ -98,12 +104,12 @@ class Experiment:
                                               ])
         
         svhn_transform = transforms.Compose(
-            [transforms.ColorJitter(hue=0.2, contrast=0.5, saturation=0.5, brightness=0.3),
-             transforms.Resize(32),
-             transforms.ToTensor(),
-             transforms.Normalize(mean=(0.4377, 0.4437, 0.4728),
-                                  std=(0.1980, 0.2010, 0.1970))
-             ])
+            [
+                # transforms.ColorJitter(hue=0.2, contrast=0.5, saturation=0.5, brightness=0.3),
+                transforms.Resize(32),
+                transforms.ToTensor(),
+                transforms.Normalize(mean=(0.4377, 0.4437, 0.4728), std=(0.1980, 0.2010, 0.1970))
+            ])
         
         self.dataset1 = datasets.MNIST(root="../data/", train=True, transform=mnist_transform, download=True)
         self.dataset2 = datasets.SVHN(root="../data/", split='train', download=True, transform=svhn_transform)
@@ -147,36 +153,41 @@ class Experiment:
                 dom_labels = torch.cat([torch.zeros(img1.size(0)), torch.ones(img2.size(0))])
                 images, dom_labels, labels1 = images.cuda(), dom_labels.cuda(), labels1.cuda()
                 
-                f, l, d = self.net.forward(images, img1.size(0))
+                p = total_batches / (len(self.loader_1) * num_epochs)
+                if self.dual_train:
+                    alfa = 2 / (1 + math.exp(-10 * p)) - 1
+                else:
+                    alfa = 0
+                
+                f, l, d = self.net.forward(images, img1.size(0), alpha=alfa)
                 ce_loss = nn.CrossEntropyLoss()(l, labels1)
                 total_batches += 1
-                p = total_batches / (len(self.loader_1) * num_epochs)
                 dom_pred = torch.sigmoid(d)
                 dom_loss = nn.BCELoss()(dom_pred, dom_labels)
-                import math
-                lmbda = 2 / (1 + math.exp(-10 * p)) - 1
-                total_loss = ce_loss + lmbda * dom_loss
-                # dom_loss = torch.tensor([0.0])
+                if self.dual_train:
+                    total_loss = ce_loss + dom_loss
+                else:
+                    total_loss = ce_loss
                 total_loss.backward()
                 tqdm_bar.set_description("Ep: {}/{}  LR: {:.4f}  CE Loss: {:.4f}  Dom Loss: {:.4f} Lmbda: {:.4f}  "
                                          "Tot Loss: {:.4f}".format(ep, num_epochs, self.optimizer.param_groups[0]['lr'],
-                                                                   ce_loss.item(), dom_loss.item(), lmbda,
+                                                                   ce_loss.item(), dom_loss.item(), alfa,
                                                                    total_loss.item()))
                 self.optimizer.step()
                 tb_writer.add_scalar(tag="LR", scalar_value=self.optimizer.param_groups[0]['lr'],
                                      global_step=total_batches)
-                tb_writer.add_scalar(tag="Lambda", scalar_value=lmbda, global_step=total_batches)
+                tb_writer.add_scalar(tag="Lambda", scalar_value=alfa, global_step=total_batches)
                 tb_writer.add_scalar(tag="p", scalar_value=p, global_step=total_batches)
                 tb_writer.add_scalars(main_tag="Training", tag_scalar_dict={'CE Loss': ce_loss.item(),
                                                                             'Dom Loss': dom_loss.item(),
                                                                             'Total Loss': total_loss.item()
                                                                             },
                                       global_step=total_batches)
-                
-            if (ep + 1) % 10 == 0:
+            
+            if (ep + 1) % 20 == 0:
                 self.optimizer.param_groups[0]['lr'] *= 0.9
         tb_writer.close()
-        
+    
     def eval(self):
         """
         Eval network on test datasets
@@ -193,7 +204,7 @@ class Experiment:
             for images, labels in loader:
                 images, labels = images.cuda(), labels.cuda()
                 with torch.no_grad():
-                    _, logits, _ = self.net.forward(images, images.size(0))
+                    _, logits, _ = self.net.forward(images, images.size(0), alpha=0)
                     res, pred = utils.calc_accuracy(output=logits, target=labels, topk=(1,))
                     total_num += logits.size(0)
                     correct_num += res[0].item() * logits.size(0)
@@ -202,6 +213,6 @@ class Experiment:
 
 
 if __name__ == "__main__":
-    exp = Experiment(train_on_svhn=True)
+    exp = Experiment(train_on_svhn=True, dual_train=True)
     exp.train()
     exp.eval()
