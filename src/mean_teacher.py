5184ab723c7b5cd97e78bdaad6af3e6f686edd6c
sanyald <deepayan.sanyal@vanderbilt.edu>
2022-08-03 14:42:45 -0500

    mean_teacher network for mnist->svhn

--- src/mean_teacher.py
+++ src/mean_teacher.py
@@ -8,18 +8,74 @@ import torchvision.transforms as transforms
 import torchvision.datasets as datasets
 import tqdm
 import torch.utils.data as torchdata
+import torch.nn.functional as F
 
 
 class Network(nn.Module):
     """
     Backbone network for mean teacher model
+    Code sourced from: https://github.com/Britefury/self-ensemble-visual-domain-adapt
     """
     
+    def __init__(self, n_classes):
+        super().__init__()
+    
+        self.conv1_1 = nn.Conv2d(3, 128, (3, 3), padding=1)
+        self.conv1_1_bn = nn.BatchNorm2d(128)
+        self.conv1_2 = nn.Conv2d(128, 128, (3, 3), padding=1)
+        self.conv1_2_bn = nn.BatchNorm2d(128)
+        self.conv1_3 = nn.Conv2d(128, 128, (3, 3), padding=1)
+        self.conv1_3_bn = nn.BatchNorm2d(128)
+        self.pool1 = nn.MaxPool2d((2, 2))
+        self.drop1 = nn.Dropout()
+    
+        self.conv2_1 = nn.Conv2d(128, 256, (3, 3), padding=1)
+        self.conv2_1_bn = nn.BatchNorm2d(256)
+        self.conv2_2 = nn.Conv2d(256, 256, (3, 3), padding=1)
+        self.conv2_2_bn = nn.BatchNorm2d(256)
+        self.conv2_3 = nn.Conv2d(256, 256, (3, 3), padding=1)
+        self.conv2_3_bn = nn.BatchNorm2d(256)
+        self.pool2 = nn.MaxPool2d((2, 2))
+        self.drop2 = nn.Dropout()
+    
+        self.conv3_1 = nn.Conv2d(256, 512, (3, 3), padding=0)
+        self.conv3_1_bn = nn.BatchNorm2d(512)
+        self.nin3_2 = nn.Conv2d(512, 256, (1, 1), padding=1)
+        self.nin3_2_bn = nn.BatchNorm2d(256)
+        self.nin3_3 = nn.Conv2d(256, 128, (1, 1), padding=1)
+        self.nin3_3_bn = nn.BatchNorm2d(128)
+    
+        self.fc4 = nn.Linear(128, n_classes)
+
+    def forward(self, x):
+        """Forward prop for the network"""
+        x = F.relu(self.conv1_1_bn(self.conv1_1(x)))
+        x = F.relu(self.conv1_2_bn(self.conv1_2(x)))
+        x = self.pool1(F.relu(self.conv1_3_bn(self.conv1_3(x))))
+        x = self.drop1(x)
+    
+        x = F.relu(self.conv2_1_bn(self.conv2_1(x)))
+        x = F.relu(self.conv2_2_bn(self.conv2_2(x)))
+        x = self.pool2(F.relu(self.conv2_3_bn(self.conv2_3(x))))
+        x = self.drop2(x)
+    
+        x = F.relu(self.conv3_1_bn(self.conv3_1(x)))
+        x = F.relu(self.nin3_2_bn(self.nin3_2(x)))
+        x = F.relu(self.nin3_3_bn(self.nin3_3(x)))
+    
+        x = F.avg_pool2d(x, 6)
+        x = x.view(-1, 128)
+    
+        x = self.fc4(x)
+        return x
+
 
 class MeanTeacher:
     """
     Module for implementing the mean teacher architecture
     """
+    def __init__(self):
+        self.net = Network()
 
     
 class Experiment:
