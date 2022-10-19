2757f0970e355bfcd5f8922537684760efb7483a
sanyald <deepayan.sanyal93@gmail.com>
2022-10-19 13:46:48 -0500

    More functionalities for experiment tracking DANN

--- src/DANN.py
+++ src/DANN.py
@@ -29,7 +29,7 @@ class Experiment:
     """
     Class used to run the experiments
     """
-    
+
     def __init__(self, exp_args):
         self.args = exp_args
         self.source_dataset = self.args['dataset_source']
@@ -43,34 +43,49 @@ class Experiment:
         self.starting_lr = self.args['starting_lr']
         self.hypertune = not self.args['final']
         self.b_size = self.args['batchsize']
-        
+
         net_args = {'backbone': self.backbone}
         self.net = DANNNetworks.get_network(source_dataset=self.source_dataset, target_dataset=self.target_dataset,
                                             args=net_args)
-        
+
         dataset_args = {'normalize': self.normalize,
                         'train': True,
                         'hypertune': self.hypertune
                         }
         self.dataset1 = DANNDatasets.prepare_dataset(self.source_dataset, args=dataset_args)
         self.dataset2 = DANNDatasets.prepare_dataset(self.target_dataset, args=dataset_args)
-        
+
         self.loader_1 = torchdata.DataLoader(self.dataset1, batch_size=self.b_size, shuffle=True, num_workers=4)
         self.loader_2 = torchdata.DataLoader(self.dataset2, batch_size=self.b_size, shuffle=True, num_workers=4)
-        
+
         dataset_args['train'] = False
         self.test_dataset1 = DANNDatasets.prepare_dataset(self.source_dataset, args=dataset_args)
         self.test_dataset2 = DANNDatasets.prepare_dataset(self.target_dataset, args=dataset_args)
-        
-        self.test_loader_1 = torchdata.DataLoader(self.test_dataset1, batch_size=2*self.b_size, num_workers=4)
-        self.test_loader_2 = torchdata.DataLoader(self.test_dataset2, batch_size=2*self.b_size, num_workers=4)
-        
+
+        self.test_loader_1 = torchdata.DataLoader(self.test_dataset1, batch_size=2 * self.b_size, num_workers=4)
+        self.test_loader_2 = torchdata.DataLoader(self.test_dataset2, batch_size=2 * self.b_size, num_workers=4)
+
         self.net = self.net.cuda()
         self.optimizer = torch.optim.SGD(self.net.backbone.parameters(), lr=self.get_lr(p=0.0), weight_decay=1e-5,
                                          momentum=0.9)
         self.optimizer.add_param_group({'params': self.net.classifier.parameters()})
         self.optimizer.add_param_group({'params': self.net.domain_classifier.parameters()})
-    
+
+        import datetime
+        self.exp_time = datetime.datetime.now()
+        runs_path = RUNS_DIR + self.source_dataset.upper() + "_" + self.target_dataset.upper() + "/exp_" \
+                             + self.exp_time.strftime("%b-%d-%Y-%H-%M") + "/"
+        self.tb_writer = tb.SummaryWriter(log_dir=runs_path)
+        self.save_args(path=runs_path)
+
+    def save_args(self, path):
+        """Save the experiment args in json file"""
+        import json
+        json_args = json.dumps(self.args)
+        out_file = open(path + "exp_args.json", "w")
+        out_file.write(json_args)
+        out_file.close()
+
     def get_lr(self, p):
         """
         Returns the lr of the current batch
@@ -84,21 +99,17 @@ class Experiment:
         else:
             lr = 0.5 * self.starting_lr * (1 + math.cos(math.pi * p))
         return lr
-    
+
     def train(self):
         """
         Train network
         """
-        import datetime
-        dt_now = datetime.datetime.now()
-        runs_path = RUNS_DIR + self.source_dataset.upper() + "_" + self.target_dataset.upper() + "/exp_" \
-                             + dt_now.strftime("%b-%d-%Y-%H-%M") + "/"
-        tb_writer = tb.SummaryWriter(log_dir=runs_path)
-        loader_2_iter = iter(self.loader_2)
         self.net.backbone.train()
         self.net.classifier.train()
         self.net.domain_classifier.train()
+
         total_batches = 0
+        loader_2_iter = iter(self.loader_2)
         for ep in range(1, self.num_epochs + 1):
             tqdm_bar = tqdm.tqdm(self.loader_1, ncols=150)
             ep_batches = 0
@@ -112,10 +123,10 @@ class Experiment:
                     loader_2_iter = iter(self.loader_2)
                     idx2, img2, labels2 = next(loader_2_iter)
                 self.optimizer.zero_grad()
-                
+
                 p = total_batches / (len(self.loader_1) * self.num_epochs)
                 alfa = 2 / (1 + math.exp(-10 * p)) - 1
-                
+
                 if self.combined_batch:
                     images = torch.concat([img1, img2], dim=0)
                     dom_labels = torch.cat([torch.zeros(img1.size(0)), torch.ones(img2.size(0))])
@@ -138,44 +149,48 @@ class Experiment:
                 dom_loss = nn.BCELoss()(dom_pred, dom_labels)
                 total_loss = ce_loss + dom_loss
                 total_loss.backward()
-                tqdm_bar.set_description("Ep: {}/{}  LR: {:.4f}  CE Loss: {:.4f}  Dom Loss: {:.4f} Lmbda: {:.4f}  "
-                                         "Tot Loss: {:.4f}".format(ep, self.num_epochs,
-                                                                   self.optimizer.param_groups[0]['lr'],
-                                                                   ce_loss.item(), dom_loss.item(), alfa,
-                                                                   total_loss.item()))
                 self.optimizer.step()
+
                 ep_batches += 1
                 ep_ce_loss += ce_loss.item()
                 ep_dom_loss += dom_loss.item()
                 ep_tot_loss += total_loss.item()
-                tb_writer.add_scalar(tag="LR", scalar_value=self.optimizer.param_groups[0]['lr'],
-                                     global_step=total_batches)
-                tb_writer.add_scalar(tag="Lambda", scalar_value=alfa, global_step=total_batches)
-                tb_writer.add_scalar(tag="p", scalar_value=p, global_step=total_batches)
-                tb_writer.add_scalars(main_tag="Training", tag_scalar_dict={'CE Loss': ce_loss.item(),
-                                                                            'Dom Loss': dom_loss.item(),
-                                                                            'Total Loss': total_loss.item()
-                                                                            },
-                                      global_step=total_batches)
-                tb_writer.add_scalars(main_tag="Training", tag_scalar_dict={'Avg CE Loss': ep_ce_loss / ep_batches,
-                                                                            'Avg Dom Loss': ep_dom_loss / ep_batches,
-                                                                            'Avg Total Loss': ep_tot_loss / ep_batches
-                                                                            },
-                                      global_step=total_batches)
+                tqdm_bar.set_description("Ep: {}/{}  LR: {:.4f}  CE Loss: {:.4f}  Dom Loss: {:.4f} Lmbda: {:.4f}  "
+                                         "Tot Loss: {:.4f}".format(ep, self.num_epochs,
+                                                                   self.optimizer.param_groups[0]['lr'],
+                                                                   ep_ce_loss/ep_batches, ep_dom_loss/ep_batches, alfa,
+                                                                   ep_tot_loss/ep_batches))
+
+                self.tb_writer.add_scalar(tag="LR", scalar_value=self.optimizer.param_groups[0]['lr'],
+                                          global_step=total_batches)
+                self.tb_writer.add_scalar(tag="Lambda", scalar_value=alfa, global_step=total_batches)
+                self.tb_writer.add_scalar(tag="p", scalar_value=p, global_step=total_batches)
+                self.tb_writer.add_scalars(main_tag="Training",
+                                           tag_scalar_dict={'CE Loss (Batch)': ce_loss.item(),
+                                                            'Dom Loss (Batch)': dom_loss.item(),
+                                                            'Total Loss (Batch)': total_loss.item()
+                                                            },
+                                           global_step=total_batches)
+                self.tb_writer.add_scalars(main_tag="Training",
+                                           tag_scalar_dict={'CE Loss (Epoch Avg)': ep_ce_loss / ep_batches,
+                                                            'Dom Loss (Epoch Avg)': ep_dom_loss / ep_batches,
+                                                            'Total Loss (Epoch Avg)': ep_tot_loss / ep_batches
+                                                            },
+                                           global_step=total_batches)
+
                 next_lr = self.get_lr(p=p)
                 for idx_group in range(len(self.optimizer.param_groups)):
                     self.optimizer.param_groups[idx_group]['lr'] = next_lr
-        
+
         if self.save:
             out_dir = OUT_DIR + self.source_dataset.upper() + "_" + self.target_dataset.upper() + "/exp_" + \
-                      dt_now.strftime("%b-%d-%Y-%H-%M") + "/"
+                      self.exp_time.strftime("%b-%d-%Y-%H-%M") + "/"
             os.makedirs(out_dir, exist_ok=False)
             print("Saving model components to {}".format(out_dir))
             torch.save(self.net.backbone.state_dict(), out_dir + "backbone_final.pt")
             torch.save(self.net.classifier.state_dict(), out_dir + "classifier_final.pt")
             torch.save(self.net.domain_classifier.state_dict(), out_dir + "domain_classifier_final.pt")
-        tb_writer.close()
-    
+
     def eval(self):
         """
         Eval network on test datasets
@@ -186,6 +201,7 @@ class Experiment:
         dataloaders = [self.loader_1, self.test_loader_1, self.loader_2, self.test_loader_2]
         d_names = [self.source_dataset + "_train", self.source_dataset + "_test", self.target_dataset + "_train",
                    self.target_dataset + "_test"]
+        accuracies = {}
         for d_idx in range(len(dataloaders)):
             loader = dataloaders[d_idx]
             total_num = 0
@@ -199,6 +215,17 @@ class Experiment:
                     correct_num += res[0].item() * logits.size(0)
             acc = correct_num / total_num
             print("Accuracy on dataset {}: {:.2f}".format(d_names[d_idx], acc))
+            accuracies[d_names[d_idx]] = acc
+            self.tb_writer.add_text(tag="Accuracy/" + d_names[d_idx], text_string=str(acc))
+        acc_file_path = RUNS_DIR + self.source_dataset.upper() + "_" + self.target_dataset.upper() + "/exp_" \
+                                 + self.exp_time.strftime("%b-%d-%Y-%H-%M") + "/acc.json"
+        import json
+        accuracies_json = json.dumps(accuracies)
+        acc_file = open(acc_file_path, "w")
+        acc_file.write(accuracies_json)
+        acc_file.close()
+
+        self.tb_writer.close()
 
 
 def get_parser():
@@ -218,7 +245,7 @@ def get_parser():
     parser.add_argument("--starting-lr", "-lr", default=0.05, type=float)
     parser.add_argument("--final", default=False, action='store_true')
     parser.add_argument("--batchsize", "-b", default=64, type=int)
-    
+
     return vars(parser.parse_args())
 
 
