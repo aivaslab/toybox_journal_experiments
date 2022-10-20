025475fceae8edfe46139885a55e58b78a67eca1
sanyald <deepayan.sanyal@vanderbilt.edu>
2022-10-20 13:49:10 -0500

    1 class for all domains in Office-31

--- src/JAN_dataset.py
+++ src/JAN_dataset.py
@@ -17,66 +17,33 @@ OFFICE31_WEBCAM_MEAN = (0.6172, 0.6187, 0.6120)
 OFFICE31_WEBCAM_STD = (0.2589, 0.2568, 0.2519)
 
 
-class Office31Amazon(torchdata.Dataset):
+class Office31(torchdata.Dataset):
     """
-    Dataset Class for Office-31 images for Amazon domain
+    Dataset Class for Office-31 images
     """
-    IMAGES_PATH = "../../DATASETS/office-31/amazon.pkl"
-    LABELS_PATH = "../../DATASETS/office-31/amazon.csv"
+    AMAZON_IMAGES_PATH = "../../DATASETS/office-31/amazon.pkl"
+    AMAZON_LABELS_PATH = "../../DATASETS/office-31/amazon.csv"
+    DSLR_IMAGES_PATH = "../../DATASETS/office-31/dslr.pkl"
+    DSLR_LABELS_PATH = "../../DATASETS/office-31/dslr.csv"
+    WEBCAM_IMAGES_PATH = "../../DATASETS/office-31/webcam.pkl"
+    WEBCAM_LABELS_PATH = "../../DATASETS/office-31/webcam.csv"
+    DOMAINS = ['amazon', 'dslr', 'webcam']
     
-    def __init__(self, transform=None):
+    def __init__(self, domain, transform=None):
+        assert domain in self.DOMAINS
+        self.domain = domain
         self.transform = transform
         
-        images_file = open(self.IMAGES_PATH, "rb")
-        labels_file = open(self.LABELS_PATH, "r")
-        self.images = pickle.load(images_file)
-        self.labels = list(csv.DictReader(labels_file))
-    
-    def __len__(self):
-        return len(self.images)
-    
-    def __getitem__(self, item):
-        img, label = np.array(cv2.imdecode(self.images[item], 3)), int(self.labels[item]['Class ID'])
-        if self.transform:
-            img = self.transform(img)
-        return item, img, label
-
-
-class Office31DSLR(torchdata.Dataset):
-    """
-    Dataset Class for Office-31 images for DSLR domain
-    """
-    IMAGES_PATH = "../../DATASETS/office-31/dslr.pkl"
-    LABELS_PATH = "../../DATASETS/office-31/dslr.csv"
-    
-    def __init__(self, transform=None):
-        self.transform = transform
-        
-        images_file = open(self.IMAGES_PATH, "rb")
-        labels_file = open(self.LABELS_PATH, "r")
-        self.images = pickle.load(images_file)
-        self.labels = list(csv.DictReader(labels_file))
-    
-    def __len__(self):
-        return len(self.images)
-    
-    def __getitem__(self, item):
-        img, label = np.array(cv2.imdecode(self.images[item], 3)), int(self.labels[item]['Class ID'])
-        if self.transform:
-            img = self.transform(img)
-        return item, img, label
-
-
-class Office31Webcam(torchdata.Dataset):
-    """
-    Dataset Class for Office-31 images for Webcam domain
-    """
-    IMAGES_PATH = "../../DATASETS/office-31/webcam.pkl"
-    LABELS_PATH = "../../DATASETS/office-31/webcam.csv"
-    
-    def __init__(self, transform=None):
-        self.transform = transform
-        
+        if self.domain == 'amazon':
+            self.IMAGES_PATH = self.AMAZON_IMAGES_PATH
+            self.LABELS_PATH = self.AMAZON_LABELS_PATH
+        elif self.domain == 'dslr':
+            self.IMAGES_PATH = self.DSLR_IMAGES_PATH
+            self.LABELS_PATH = self.DSLR_LABELS_PATH
+        else:
+            self.IMAGES_PATH = self.WEBCAM_IMAGES_PATH
+            self.LABELS_PATH = self.WEBCAM_LABELS_PATH
+            
         images_file = open(self.IMAGES_PATH, "rb")
         labels_file = open(self.LABELS_PATH, "r")
         self.images = pickle.load(images_file)
@@ -96,7 +63,7 @@ if __name__ == "__main__":
     trnsfrm = transforms.Compose([transforms.ToPILImage(), transforms.ToTensor(),
                                   transforms.Resize(224),
                                   transforms.Normalize(mean=OFFICE31_AMAZON_MEAN, std=OFFICE31_AMAZON_STD)])
-    dataset = Office31Amazon(transform=trnsfrm)
+    dataset = Office31(domain='amazon', transform=trnsfrm)
     print(len(dataset))
     dataloader = torchdata.DataLoader(dataset, batch_size=64, shuffle=False)
     print(utils.online_mean_and_sd(dataloader))
@@ -104,7 +71,7 @@ if __name__ == "__main__":
     trnsfrm = transforms.Compose([transforms.ToPILImage(), transforms.ToTensor(),
                                   transforms.Resize(224),
                                   transforms.Normalize(mean=OFFICE31_DSLR_MEAN, std=OFFICE31_DSLR_STD)])
-    dataset = Office31DSLR(transform=trnsfrm)
+    dataset = Office31(domain='dslr', transform=trnsfrm)
     print(len(dataset))
     dataloader = torchdata.DataLoader(dataset, batch_size=64, shuffle=False)
     print(utils.online_mean_and_sd(dataloader))
@@ -112,7 +79,7 @@ if __name__ == "__main__":
     trnsfrm = transforms.Compose([transforms.ToPILImage(), transforms.ToTensor(),
                                   transforms.Resize(224),
                                   transforms.Normalize(mean=OFFICE31_WEBCAM_MEAN, std=OFFICE31_WEBCAM_STD)])
-    dataset = Office31Webcam(transform=trnsfrm)
+    dataset = Office31(domain='webcam', transform=trnsfrm)
     print(len(dataset))
     dataloader = torchdata.DataLoader(dataset, batch_size=64, shuffle=False)
     print(utils.online_mean_and_sd(dataloader))
