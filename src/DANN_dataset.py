8289229ac97c6225c4d3fe4d4d248c9a5f4c5151
sanyald <deepayan.sanyal@vanderbilt.edu>
2022-11-08 17:06:21 -0600

    reverse mean, std for bgr2rgb

--- src/DANN_dataset.py
+++ src/DANN_dataset.py
@@ -25,11 +25,11 @@ SVHN_STD = (0.1980, 0.2010, 0.1970)
 MNISTM_MEAN = (0.4082, 0.4621, 0.4579)
 MNISTM_STD = (0.2587, 0.2368, 0.2519)
 
-IN12_MEAN = (0.4541, 0.4845, 0.4980)
-IN12_STD = (0.2928, 0.2738, 0.2756)
+IN12_MEAN = (0.4980, 0.4845, 0.4541)
+IN12_STD = (0.2756, 0.2738, 0.2928)
 
-TOYBOX_MEAN = (0.3499, 0.4374, 0.5199)
-TOYBOX_STD = (0.1623, 0.1894, 0.1775)
+TOYBOX_MEAN = (0.5199, 0.4374, 0.3499)
+TOYBOX_STD = (0.1775, 0.1894, 0.1623)
 
 TOYBOX_DATA_PATH = "../data_12/Toybox/"
 IN12_DATA_PATH = "../data_12/IN-12/"
@@ -111,11 +111,11 @@ def get_transform(dataset, mean, std, train):
 def get_dataset(d_name, args):
     """Return the class signature of Dataset"""
     if d_name == 'mnist':
-        return DatasetMNIST(transform=args['transform'], train=args['train'])
+        return DatasetMNIST(transform=args['transform'], train=args['train'], special_aug=args['special_aug'])
     if d_name == 'mnist50':
         return DatasetMNIST50(train=args['train'], transform=args['transform'], special_aug=args['special_aug'])
     if d_name == 'svhn':
-        return DatasetSVHN(transform=args['transform'], train=args['train'])
+        return DatasetSVHN(transform=args['transform'], train=args['train'], special_aug=args['special_aug'])
     if d_name == 'svhn-b':
         return DatasetSVHNB(train=args['train'], transform=args['transform'], hypertune=args['hypertune'],
                             special_aug=args['special_aug'])
@@ -277,17 +277,21 @@ class DatasetMNIST(torchdata.Dataset):
     """
     Dataset Class for MNIST
     """
-    def __init__(self, train, transform):
+    def __init__(self, train, transform, special_aug):
         self.train = train
         self.transform = transform
         self.data = datasets.MNIST(root="../data/", train=self.train, transform=self.transform, download=True)
+        self.special_aug = special_aug
+        self.aug = self_ensemble_aug.get_aug_for_mnist()
     
     def __len__(self):
         return len(self.data)
     
     def __getitem__(self, item):
         img, label = self.data[item]
-        return item, img, label
+        if self.special_aug and self.train is True:
+            img = self.aug.augment(img.unsqueeze(0))
+        return item, img.squeeze(), label
 
     def __str__(self):
         return "MNIST"
@@ -298,18 +302,22 @@ class DatasetSVHN(torchdata.Dataset):
     Dataset Class for SVHN
     """
     
-    def __init__(self, train, transform):
+    def __init__(self, train, transform, special_aug):
         self.train = train
         self.split = 'train' if self.train else 'test'
         self.transform = transform
         self.data = datasets.SVHN(root="../data/", split=self.split, transform=self.transform, download=True)
+        self.special_aug = special_aug
+        self.aug = self_ensemble_aug.get_aug_for_mnist()
     
     def __len__(self):
         return len(self.data)
     
     def __getitem__(self, item):
         img, label = self.data[item]
-        return item, img, label
+        if self.special_aug and self.train is True:
+            img = self.aug.augment(img.unsqueeze(0))
+        return item, img.squeeze(), label
     
     def __str__(self):
         return "SVHN"
